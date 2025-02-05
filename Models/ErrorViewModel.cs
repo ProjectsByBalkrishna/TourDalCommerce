032ac789f7e75a219e6dea332c7989a81fa084bc
Balkrishna-1663 <Kishansingh223311@gmail.com>
2025-02-06 00:48:21 +0530

    Created Project and Plan

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tourdalCommerce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
